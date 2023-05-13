3e0404dee8dafec6259522ccde361e0930cc417e
nikartik <110419961+NikartikTheBear@users.noreply.github.com>
2023-05-13 17:05:57 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,14 +1,16 @@
+
+
 RegisterCommand("wipe", function (source, args, rawCommand)
-    if source ~= nil then
+    local source = source
         local xPlayer = ESX.GetPlayerFromId(source)  
         if xPlayer.GetGroup() == "admin" then
             if args ~= nil  then
                 if args[1]  ~= nil then
-                    MySQL.Async.fetchAll("SELECT identifier FROM users WHERE identifier =@id", {
+                    MySQL.query("SELECT identifier FROM users WHERE identifier =@id", {
                         ["@id"] = args[1]
                     }, function (result)
                         if #result ~= 0 then
-                            MySQL.Async.execute("DELETE FROM users WHERE identifier=@identifier", { ["@identifier"] = result[1].identifier })
+                            MySQL.query("DELETE FROM users WHERE identifier=@identifier", { ["@identifier"] = result[1].identifier })
 
                             xPlayer.showNotification("Wipe avvenuto con successo")
                             
@@ -19,7 +21,6 @@ RegisterCommand("wipe", function (source, args, rawCommand)
                 end
             end 
         else
-            return
+            xPlayer.showNotification('Specifica un id')
         end
-    end
 end, false)
